eb26a3338091e5dba69fd046d782d22d875db151
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-04-06 16:33:49 -0500

    Add files via upload

--- hentaibase.js
+++ hentaibase.js
@@ -287,6 +287,373 @@ const baseDeDatos = [{
     tags: ["Escolares", "Vanilla", "Uncensored"],
     tipo: "sincensura",
     descripcion: "Asuka ha estado teniendo sexo por internet con su amigo de la infancia, Ryou, pero le molesta que él no se haya dado cuenta de que es ella, hasta que se lo confiesa sin saberlo."
-}
+},
+{
+    id: "H15",
+    titulo: "Uba: Milk Honey",
+    poster: "https://iili.io/qTeWU3g.png",
+    bg: [
+        "https://iili.io/qTeWNjV.jpg",
+        "https://iili.io/qTeWOZB.jpg",
+        "https://iili.io/qTeWwTQ.jpg",
+        "https://iili.io/qTeWhyx.jpg"
+    ],
+    video: [
+        "https://www.burstcloud.co/embed/42be061fcc188ea53046707c2057a7d7006a8337adf85a3046bd30ed991c6b1f/ep1.mp4",
+        "https://www.burstcloud.co/embed/ff37c0e4d2700f2c5ccd3c001061044cac077d49faf1c022ebb458dcb937d91c/ep2.mp4"
+    ],
+    estudio: "Vanilla",
+    fecha: "2020",
+    tags: ["Milfs", "Ninfomana", "Tetonas"],
+    tipo: "sincensura",
+    descripcion: "Kyoko revive su relación con Toji, el antiguo niño al que amamantó, mientras su hija Marika también se siente atraída por él. Cuando Marika le exige elegir, descubre el lado más oscuro de su madre."
+},
+{
+    id: "H16",
+    titulo: "Gogo no Kouchou Junai Mellow yori",
+    poster: "https://iili.io/qAFBUl4.png",
+    bg: [
+        "https://iili.io/qAFBLxe.png",
+        "https://iili.io/qAFBZib.png",
+        "https://iili.io/qAFCzbt.png",
+        "https://iili.io/qAFCfqJ.png"
+    ],
+    video: [
+        "https://www.burstcloud.co/embed/821b29e25d353dee224273241b84fd908d3871e9e7c4b717c4a5beb508f020c3/ep1.mp4"
+    ],
+    estudio: "Pashmina",
+    fecha: "2020",
+    tags: ["Anal", "Ninfomana", "Sin censura"],
+    tipo: "sincensura",
+    descripcion: "Yayoi, ama de casa frustrada por la ausencia de su esposo, comienza a fantasear y sucumbe a la atracción de un joven del barrio."
+},
+{
+    id: "H17",
+    titulo: "Shounen ga Otona ni Natta Natsu",
+    poster: "https://iili.io/qTkIBJn.jpg",
+    bg: [
+        "https://iili.io/qTkzioQ.png",
+        "https://iili.io/qTkzZKP.png",
+        "https://iili.io/qTkzmHg.png",
+        "https://iili.io/qTkI9Dv.png"
+    ],
+    video: [
+        "https://www.burstcloud.co/embed/ce13ed4585afc8ef4d8998abf84a0968dd23f1dac02b32cb24cae1aff91ed683/ep1.mp4",
+        "https://www.burstcloud.co/embed/8b6801b7327624ce1fa63c1d40e6bc8a713edc778138c13605403aa1118e5448/ep2.mp4",
+        "https://www.burstcloud.co/embed/f3263e9ab4a9f8a510fe361b062d3d9d42abbd73198b6d4c35ac6b30fb0049f7/ep3.mp4",
+        "https://www.burstcloud.co/embed/6a933e68213797b8145d83a73874f2114aa0b58d8cc79e054c4ed65bf3f2ffc0/ep4.mp4"
+    ],
+    estudio: "Queen Bee",
+    fecha: "2025",
+    tags: ["Corridas", "Masturbacion", "Tetonas"],
+    tipo: "censurado",
+    descripcion: "Kirishima Ryuki se da cuenta de que Reiko nunca le mostró su habitación. Al abrir la puerta, descubre su verdadera naturaleza. Sus sentimientos crecen y ya no puede regresar. Su secreto persistirá en la adultez."
+},
+{
+    id: "H18",
+    titulo: "Nikuen",
+    poster: "",
+    bg: [
+        "https://iili.io/qTkgVCg.png",
+        "https://iili.io/qTkgcvV.png",
+        "https://iili.io/qTkgwaR.png",
+        "https://iili.io/qTkgOyN.png"
+    ],
+    video: [
+        "https://hentaiplayer.com/v/B3CHj2PFl8Wij3I/",
+        "https://hentaiplayer.com/v/60zyafFklzS2kcy/"
+    ],
+    estudio: "Queen Bee",
+    fecha: "2025",
+    tags: ["Escolares", "Milfs", "Tetonas"],
+    tipo: "censurado",
+    descripcion: "El protagonista sale con Aoi. Visitan su casa y ven a su madre con un desconocido. Aoi no se escandaliza y van a su habitación. El desconocido hipnotizó a la madre con una aplicación."
+},
+{
+    id: "H18",
+    titulo: "Nikuen",
+    poster: "https://iili.io/qTkgUnn.md.png",
+    bg: [
+        "https://iili.io/qTkgVCg.png",
+        "https://iili.io/qTkgcvV.png",
+        "https://iili.io/qTkgwaR.png",
+        "https://iili.io/qTkgOyN.png"
+    ],
+    video: [
+        "https://hentaiplayer.com/v/B3CHj2PFl8Wij3I/",
+        "https://hentaiplayer.com/v/60zyafFklzS2kcy/"
+    ],
+    estudio: "Queen Bee",
+    fecha: "2025",
+    tags: ["Escolares", "Milfs", "Tetonas"],
+    tipo: "censurado",
+    descripcion: "El protagonista sale con Aoi. Visitan su casa y ven a su madre con un desconocido. Aoi no se escandaliza y van a su habitación. El desconocido hipnotizó a la madre con una aplicación."
+},
+{
+    id: "H19",
+    titulo: "Youkoso! Sukebe Elf no Mori e",
+    poster: "https://iili.io/quXBgsI.jpg",
+    bg: [
+        "https://iili.io/quXBhf1.jpg",
+        "https://iili.io/quXBVWB.jpg",
+        "https://iili.io/quXB1Ox.jpg",
+        "https://iili.io/quXBMxV.jpg"
+    ],
+    video: [
+        "https://www.burstcloud.co/embed/fa3e30df96418c53acd77974caccff0374350e00174ab10a74be179aca150049/ep1.mp4",
+        "https://www.burstcloud.co/embed/8cce7596d426977b261076bea2613df30096d10e83a62f152f4e7005439becd3/ep2.mp4",
+        "https://www.burstcloud.co/embed/635b78033d901df06271c735f8ebcee129ab632594e057f9c587927cfcbd13b8/ep3.mp4",
+        "https://www.burstcloud.co/embed/941a1641c88f23b31766fd709b1fa27c27fcf024239bd356f6ec5e4c5d8f2e3a/ep4.mp4"
+    ],
+    estudio: "Bunnywalker",
+    fecha: "2020",
+    tags: ["Harem", "Lolicon", "Tetonas"],
+    tipo: "censurado",
+    descripcion: "Alfheim, un mundo solo de elfas, está en peligro cuando el Árbol Sagrado se debilita. Un hombre de otro mundo llega como su posible salvación."
+},
+{
+    id: "H20",
+    titulo: "1 Funkan dake Furete mo Ii yo",
+    poster: "https://iili.io/quwAzZX.png",
+    bg: [
+        "https://iili.io/qTKhBwX.jpg",
+        "https://iili.io/quwA5Ff.png",
+        "https://iili.io/quwABvp.jpg"
+    ],
+    video: [
+        "https://hentaiplayer.com/v/tydcRZBp4g882vR/",
+        "https://hentaiplayer.com/v/GmY5lDQhKbcqiYZ/",
+        "https://hentaiplayer.com/v/Z6dFpUDDfjCpdqn/"
+    ],
+    estudio: "Suiseisha",
+    fecha: "2025",
+    tags: ["Corridas", "Tentonas", "Uncensored"],
+    tipo: "censurado",
+    descripcion: "Después de mudarse, Ryota enfrenta la rutina impuesta por sus compañeras, hasta que le ofrecen un trato tentador por cada minuto de trabajo."
+},{
+    id: "H21",
+    titulo: "Shiranai Koto Shiritai no",
+    poster: "https://iili.io/quNa1f9.png",
+    bg: [
+        "https://iili.io/quNaIln.webp",
+        "https://iili.io/quNaRRf.png",
+        "https://iili.io/quNacVS.png",
+        "https://iili.io/quNa8Ug.png"
+    ],
+    video: [
+        "https://hentaiplayer.com/v/VBbVb4rQWySNq2g/",
+        "https://hentaiplayer.com/v/wxClGdykWPcPwg1/"
+    ],
+    estudio: "Suiseisha",
+    fecha: "2025",
+    tags: ["Corridas", "Sexo Oral", "Tetonas"],
+    tipo: "censurado",
+    descripcion: "El protagonista reabre la tienda de Model Kits de su abuelo con Risa, una clienta entusiasta. Un accidente con un solvente especial transforma a Risa, despertando actitudes seductoras y un deseo inesperado entre ambos, desatando una intensa conexión."
+},
+{
+    id: "H22",
+    titulo: "Kozukuri Bu!",
+    poster: "https://iili.io/quegdCl.png",
+    bg: [
+        "https://iili.io/queUUkg.png",
+        "https://iili.io/queUSYF.png",
+        "https://iili.io/queUt2t.png",
+        "https://iili.io/queU4TJ.png"
+    ],
+    video: [
+        "https://www.burstcloud.co/embed/c55d0406f6007c2ce719bba828dfb1dfc81e5c4b2d928de351b5ed43788eb2df/ep1.mp4",
+        "https://hentaiplayer.com/v/2O5HBIzLNgrzCmH/"
+    ],
+    estudio: "Mary Jane",
+    fecha: "2023",
+    tags: ["Corridas", "Escolares", "Tetonas"],
+    tipo: "censurado",
+    descripcion: "Y en su escuela hay un club para hacer realidad su sueño: el Club de Fertilidad y Parto. Hay dos chicas que quieren ser sus compañeras: Tono Umi y Asagi Aika. Umi y Aika se turnan para pasar cada día con Aoi durante el periodo de prueba."
+},
+{
+    id: "H22",
+    titulo: "Yumemiru Otome",
+    poster: "https://iili.io/quvxHhP.png",
+    bg: [
+        "https://iili.io/quvobYx.jpg",
+        "https://iili.io/quvxoTN.png",
+        "https://iili.io/quvx9TB.jpg",
+        "https://iili.io/quvx53G.png"
+    ],
+    video: [
+        "https://www.burstcloud.co/embed/d167cec5798665a35b3d557ae5101327fb6c46785a02bd50f484316c1008c03c/ep1.mp4",
+        "https://www.burstcloud.co/embed/e0ce2c7c1bade969b75bee9fbafcc4db4145f24a5be30204ecac81cf96195185/ep2.mp4",
+        "https://hentaiplayer.com/v/7z0LQSaxBiKlu3w/"
+    ],
+    estudio: "T-Rex",
+    fecha: "2024",
+    tags: ["Corridas", "Escolares", "Tetonas"],
+    tipo: "censurado",
+    descripcion: "Miyama Manaka, estudiante de la Academia Seiho, mantiene una relación romántica y sexual con Tonami-sensei. Un día, él la lleva a un piso de alquiler donde les esperan viejos amigos."
+},
+{
+    id: "H23",
+    titulo: "Hitoriga",
+    poster: "https://iili.io/quZa8J9.jpg",
+    bg: [
+        "https://iili.io/quZagDb.jpg",
+        "https://iili.io/quZaPiQ.jpg",
+        "https://iili.io/quZaQUP.jpg",
+        "https://iili.io/quZa6Wx.jpg"
+    ],
+    video: [
+        "https://www.burstcloud.co/embed/c174c5fdacbfbf2107abc641d671e4981c0195583ecaf1a7b6c302280d07d718/file.mp4",
+        "https://www.burstcloud.co/embed/662cbf3357adf94d286a7ba1efde9616697e40e64066799c0a612a931483687f/file.mp4",
+        "https://www.burstcloud.co/embed/3d36099b47eee1793f4c83c9127a18080f53f0829a39a9aebcdbfaac4b6a8805/file.mp4",
+        "https://www.burstcloud.co/embed/761836c81c65e1d6d18789e1a6e7df0a8419e235e48cb95fdccbd3fc4406f439/file.mp4"
+    ],
+    estudio: "Pink Pineapple",
+    fecha: "2020",
+    tags: ["Anal", "Escolares", "Violacion"],
+    tipo: "censurado",
+    descripcion: "Sumire Shirakawa es una chica inteligente, recta y muy reservada. Cuando de repente empieza a tener visiones y recuerdos de ella teniendo sexo en público y con su maestro. Poco a poco, caerá en la perversión y la lujuria que su maestro le ha implantado por el uso de drogas y acoso"
+},
+{
+    id: "H24",
+    titulo: "Yuusha hime Miria",
+    poster: "https://iili.io/fTVPRuj.md.webp",
+    bg: [
+        "https://iili.io/fTVPNMN.png",
+        "https://iili.io/fTVPEcF.png",
+        "https://iili.io/fTVPXwv.png",
+        "https://iili.io/fTVPhtR.png"
+    ],
+    video: [
+        "https://www.burstcloud.co/embed/cb854f456b022a3386f2f27341242fa5ce55593b2c29ab827231572cc0a780d6/ep1.mp4",
+        "https://www.burstcloud.co/embed/02a2a31afc2cc31bb580a127e602a66317b27c45045562d1acd4a4a93558ac3a/ep2.mp4",
+        "https://www.burstcloud.co/embed/450220da4704890bd05a5ae911288e81cb95aac7e5b01c7ef24d023fbb58304f/EP3.mp4",
+        "https://hentaiplayer.com/v/MnMc3AAAzGmnLam/"
+    ],
+    estudio: "Majin Petit",
+    fecha: "2020",
+    tags: ["Anal", "Escolares", "Monstruos"],
+    tipo: "censurado",
+    descripcion: "Rick, a un día de convertirse en héroe, es víctima de una trampa urdida por Milia, la hija adoptiva de su padre. Al ser ejecutado por atacar a Milia, el Rey Demonio lo revive como espíritu para vengarse y manipular su mente."
+},
+{
+    id: "H25",
+    titulo: "Bishoku-ke no Rule",
+    poster: "https://iili.io/qzysTNf.jpg",
+    bg: [
+        "https://iili.io/qzys7PS.jpg",
+        "https://iili.io/qzys5V2.jpg",
+        "https://iili.io/qzyslSe.jpg",
+        "https://iili.io/qzys1Hu.png"
+    ],
+    video: [
+        "https://hentaiplayer.com/v/ACV3i9tNx9S35S3/",
+        "https://hentaiplayer.com/v/bub8o4MEuRXhUTv/"
+    ],
+    estudio: "—",
+    fecha: "2020",
+    tags: ["Maids", "Escolares", "Violación"],
+    tipo: "censurado",
+    descripcion: "Reina se muda a vivir con su tío tras una serie de cambios en su vida. Aunque al principio mantiene una actitud distante y fría, su relación da un giro cuando descubre que él es su mayor apoyo emocional y financiero. Siguiendo su consejo, Reina comienza a trabajar en un café de maids, un entorno que no solo le permite ganar dinero, sino también explorar facetas de su personalidad y sexualidad que mantenía ocultas bajo una estricta disciplina escolar."
+},
+{
+    id: "H26",
+    titulo: "Yamitsuki Pheromone",
+    poster: "https://iili.io/qubZR5b.jpg",
+    bg: [
+        "https://iili.io/qubZqb4.jpg",
+        "https://iili.io/qubZnX2.png",
+        "https://iili.io/qubZAdu.jpg",
+        "https://iili.io/qubZ7mx.jpg"
+    ],
+    video: [
+        "https://www.burstcloud.co/embed/68bf3009f64f0be3c8a639670c7bdc324619063ac01cf30ab2e6ceafd37ca609/ep1.mp4",
+        "https://www.burstcloud.co/embed/60de03f9f175b66f93f836581fbfcbafe7d1c3a1fba75dd9ef1051fc5fd28e78/ep2.mp4"
+    ],
+    estudio: "N/A",
+    fecha: "N/A",
+    tags: ["Escolares", "Harem", "Tetonas"],
+    tipo: "censurado",
+    descripcion: "4 mini historias donde un bato se coge a una elfa diferente de su escuela."
+},
+{
+    id: "H27",
+    titulo: "Yarichin Kateikyoushi Netori Koukoku",
+    poster: "https://iili.io/qAjDzKP.png",
+    bg: [
+        "https://iili.io/qAjDqDx.jpg",
+        "https://iili.io/qAjDfNj.jpg",
+        "https://iili.io/qAjDFHu.webp",
+        "https://iili.io/qAjD2Se.jpg"
+    ],
+    video: [
+        "https://www.burstcloud.co/embed/72b0df4ec2f4e86afe5203da0882c0979126bec56d261260caf5256a468e3299/ep1.mp4",
+        "https://www.burstcloud.co/embed/027e38dc46a40fc59b95cd27559cbcaea1b296600da77c97f740e1d72399f51c/ep2.mp4"
+    ],
+    estudio: "T-Rex",
+    fecha: "2020",
+    tags: ["Milfs", "Netorare", "Tetonas"],
+    tipo: "censurado",
+    descripcion: "El NTR continúa. Ahora en vez de una a la vez ha seducido a la madre y a la hija juntas para que se vayan y estén con él."
+},
+{
+    id: "H28",
+    titulo: "Yamitsuki Mura: Melty Limit",
+    poster: "https://iili.io/qAwMU3g.png",
+    bg: [
+        "https://iili.io/qAwMhyx.jpg",
+        "https://iili.io/qAwMWYb.jpg",
+        "https://iili.io/qAwMvG1.jpg",
+        "https://iili.io/qAwMr8J.png"
+    ],
+    video: [
+        "https://www.burstcloud.co/embed/f0f47b8995deb6ce1b146bd96ac856a8d02977cdf6c1bb18f7da08e3cf790b19/ep1.mp4",
+        "https://www.burstcloud.co/embed/f973549b0a22d083ae1528cd2ed1e2735fac7a6e41f58aa0f0e2cdae05af350d/ep2.mp4"
+    ],
+    estudio: "Showten",
+    fecha: "2022",
+    tags: ["Anal", "Succubus", "Tetonas"],
+    tipo: "censurado",
+    descripcion: "El NTR continúa. Ahora en vez de una a la vez ha seducido a la madre y a la hija juntas para que se vayan y estén con él."
+},
+{
+    id: "H29",
+    titulo: "Toriko no Kusari",
+    poster: "https://iili.io/qAOWAzv.jpg",
+    bg: [
+        "https://iili.io/qAOWYqN.jpg",
+        "https://iili.io/qAOW5sp.jpg",
+        "https://iili.io/qAOWRXR.jpg",
+        "https://iili.io/qAOWGmG.jpg"
+    ],
+    video: [
+        "https://www.burstcloud.co/embed/2a4dcbaa3b031b2e703186080b79be4172901a9a252799c7befaccbbc5f1c118/ep1.mp4",
+        "https://www.burstcloud.co/embed/ae49bf62a6a5937126e424f23c425dd5919a4239d9c2bec3e0a0cc2592bac643/ep2.mp4"
+    ],
+    estudio: "Mary Jane",
+    fecha: "2020",
+    tags: ["Escolares", "Maid", "Violacion"],
+    tipo: "censurado",
+    descripcion: "Yunoki Mio es una estudiante destacada y presidenta del consejo estudiantil. Pasa tiempo con su amiga Rinka, su prima Yui y Konomi. Sus padres trabajan en el extranjero, pero no se siente sola por su ayudante Shiori."
+},
+{
+    id: "H30",
+    titulo: "Yuusha chan no Bouken wa Owatteshimatta",
+    poster: "https://iili.io/q5JOGef.png",
+    bg: [
+        "https://iili.io/q5JOc0X.jpg",
+        "https://iili.io/q5JO1Js.jpg",
+        "https://iili.io/q5JO8ej.webp",
+        "https://iili.io/q5JOhsS.jpg"
+    ],
+    video: [
+        "https://www.burstcloud.co/embed/ffc1f39520ef13a45b34c0c407bd60f38f5a53f5f911604b6b3088517dc711d5/ep1.mp4",
+        "https://hentaiplayer.com/v/laj98W08DvxUawR/"
+    ],
+    estudio: "Majin Petit",
+    fecha: "2025",
+    tags: ["Corridas", "Tentaculos", "Tetonas"],
+    tipo: "censurado",
+    descripcion: "La heroína que venció al rey demonio se ha vuelto arrogante y sufre pesadillas perturbadoras."
+},
 
 ];
