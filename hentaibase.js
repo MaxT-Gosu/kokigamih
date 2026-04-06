362490c73f42e6767bda44e2193b116ca94f2d1e
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-04-06 12:32:14 -0500

    Add files via upload

--- hentaibase.js
+++ hentaibase.js
@@ -1,7 +1,6 @@
-const baseDeDatos = [
-    { 
-        id: "H1", 
-        titulo: "Bakunyuu Oyako", 
+const baseDeDatos = [{
+        id: "H1",
+        titulo: "Bakunyuu Oyako",
         poster: "https://iili.io/qxaeW67.png",
         bg: [
             "https://iili.io/qxaeW67.png",
@@ -12,17 +11,16 @@ const baseDeDatos = [
         video: [
             "https://www.burstcloud.co/embed/a02abafa62c58e667405ea44b4f81b41055c932a0e2d6c7de236ae3a5a6381d2/ep1.mp4",
             "https://www.burstcloud.co/embed/91aa09a728bb91e9e7cf63702963042201ea0a8308cd677784b99d18899671a7/ep2.mp4"
-        ], 
+        ],
         estudio: "Y.O.U.C.",
-        fecha: "2020", 
-        tags: ["Maid", "Milf", "Sincensura"], 
-        tipo: "sincensura", 
+        fecha: "2020",
+        tags: ["Maid", "Milf", "Sincensura"],
+        tipo: "sincensura",
         descripcion: "Yusuke se enfrenta a un dilema: su tutoría con la voluptuosa Reina se complica cuando su madre, aún más exuberante, también lo desea. Ambas luchan por ganarse su amor."
-    },
-	{ 
-        id: "H2", 
-        titulo: "Nightmare x Deathscythe", 
-        poster: "https://iili.io/qI9Oq9S.png", // Imagen para los catálogos
+    }, {
+        id: "H2",
+        titulo: "Nightmare x Deathscythe",
+        poster: "https://iili.io/qI9Oq9S.png",
         bg: [
             "https://iili.io/qI9Oq9S.png",
             "https://iili.io/qI9O3F4.png",
@@ -32,17 +30,16 @@ const baseDeDatos = [
         video: [
             "https://www.burstcloud.co/embed/86f1f74015c506d3026020ba601b605c1fd2dab9fbc9b75d9f2673fa54601f4b/ep1.mp4",
             "https://www.burstcloud.co/embed/141f8dc8de4ba851b617aed08594b5c53ce728f4338a9bd2fdc73a42356d7ea9/ep2.mp4"
-        ], 
+        ],
         estudio: "MediaBank",
-        fecha: "2023", 
-        tags: ["Bondage", "Harem", "Orgias", "Sincensura"], 
-        tipo: "sincensura", 
+        fecha: "2023",
+        tags: ["Bondage", "Harem", "Orgias", "Sincensura"],
+        tipo: "sincensura",
         descripcion: "Natsume Rinne, una colegiala que despierta como Shinigami tras vengar a su padre, es capturada por una secta demoníaca. Cuando está a punto de ser sacrificada, las exorcistas Setsuna y Yuuka intentan salvarla, pero son rodeadas."
-    },
-	{ 
-        id: "H3", 
-        titulo: "Seiyoku Tsuyo Tsuyo", 
-        poster: "https://iili.io/qI9yHkN.png", // Imagen para los catálogos
+    }, {
+        id: "H3",
+        titulo: "Seiyoku Tsuyo Tsuyo",
+        poster: "https://iili.io/qI9yHkN.png",
         bg: [
             "https://iili.io/qI9yHkN.png",
             "https://iili.io/qI9y2It.png",
@@ -52,17 +49,16 @@ const baseDeDatos = [
         video: [
             "https://www.burstcloud.co/embed/6be4adad6db4ab45268fe42af5211fd044449d0edcbb7b697f8eddaa9dffb91d/ep1.mp4",
             "https://www.burstcloud.co/embed/d0860a1e261be788c9a5b007a4928b0da55685edc58e0012f73ac3f691c6f815/ep2.mp4"
-        ], 
+        ],
         estudio: "Pink Pineapple",
-        fecha: "2024", 
-        tags: ["Escolares", "Netorare", "Tetonas", "Sincensura"], 
-        tipo: "sincensura", 
+        fecha: "2024",
+        tags: ["Escolares", "Netorare", "Tetonas", "Sincensura"],
+        tipo: "sincensura",
         descripcion: "Nanase, una chica gal insatisfecha con su vida sexual, se burla de Nozaki, su compañero otaku. Planea humillarlo haciéndole mostrar su miembro, pero queda impactada por su tamaño. Su deseo crece y termina usándolo para su propio placer."
-    },
-	{ 
-        id: "H4", 
-        titulo: "Natsu ga Owaru made: Natsu no Owari", 
-        poster: "https://iili.io/qIHu7b1.png", // Imagen vertical para los catálogos
+    }, {
+        id: "H4",
+        titulo: "Natsu ga Owaru made: Natsu no Owari",
+        poster: "https://iili.io/qIHu7b1.png",
         bg: [
             "https://iili.io/qIHu7b1.png",
             "https://iili.io/qIHu5OP.png",
@@ -72,17 +68,16 @@ const baseDeDatos = [
         video: [
             "https://www.burstcloud.co/embed/774c0ef44b5268c23022b196e4d4d768e13c47b993dc1c1be5ed1fd9df8e8cf5/ep1.mp4",
             "https://www.burstcloud.co/embed/40dc0ac0bb8b69bff8f5ff82c2b41aae3ae5a9f8ccfb7e54e11d2feec8d41b9a/ep2.mp4"
-        ], 
+        ],
         estudio: "Showten",
-        fecha: "2024", 
-        tags: ["Anal", "Netorare", "Tetonas", "Sincensura"], 
-        tipo: "sincensura", 
+        fecha: "2024",
+        tags: ["Anal", "Netorare", "Tetonas", "Sincensura"],
+        tipo: "sincensura",
         descripcion: "Es la historia de Yui, una colegiala que acepta el acoso sexual de su maestro pervertido para proteger el sueño de su novio, Kou. Entre la humillación, el sufrimiento y el placer, Yui se convirtió gradualmente en un mero objeto sexual lujurioso."
-    },
-	{ 
-        id: "sefri-no-senpai", 
-        titulo: "SeFri no Senpai", 
-        poster: "https://iili.io/qIHvhpp.jpg", // Imagen vertical para catálogos
+    }, {
+        id: "H5",
+        titulo: "SeFri no Senpai",
+        poster: "https://iili.io/qIHvhpp.jpg",
         bg: [
             "https://iili.io/qIHvhpp.jpg",
             "https://iili.io/qIHvXvR.jpg",
@@ -92,11 +87,105 @@ const baseDeDatos = [
         video: [
             "https://www.burstcloud.co/embed/083a2d152d8fa127205f002ccdb791eb207500a4287cfe147aacbf16396be0c5/ep1.mp4",
             "https://www.burstcloud.co/embed/0cff9c7f17ba2493a302ea014c2310dbb7dc5071153a07a55459777ea1bc966a/ep2.mp4"
-        ], 
+        ],
         estudio: "Pink Pineapple",
-        fecha: "2024", 
-        tags: ["Fantasia", "Oral", "Virgenes", "Sincensura"], 
-        tipo: "sincensura", 
+        fecha: "2024",
+        tags: ["Fantasia", "Oral", "Virgenes", "Sincensura"],
+        tipo: "sincensura",
         descripcion: "Yuuma, un joven universitario, conoce a Nana en su ceremonia de bienvenida. Esa noche, ella aparece en su departamento con la intención de quitarle la virginidad."
+    }, {
+        id: "H6",
+        titulo: "Mebuki",
+        poster: "https://iili.io/qIJmANI.png",
+        bg: [
+            "https://iili.io/qIJmANI.png",
+            "https://iili.io/qIJmGHl.png",
+            "https://iili.io/qIJmYVn.png",
+            "https://iili.io/qIJmVNS.png"
+        ],
+        video: [
+            "https://hentaiplayer.com/v/PLAokCaTDiOWXRS/",
+            "https://www.burstcloud.co/embed/6a07ac9c8aae31d5d1a1d5850bd12a9b86699ec0a0cc3552f9736f17916c141e/ep2.mp4"
+        ],
+        estudio: "Pink Pineapple",
+        fecha: "2024",
+        tags: ["Corridas", "Masturbacion", "Violacion", "Sincensura"],
+        tipo: "sincensura",
+        descripcion: "«Love-chan», una streamer en declive, gana popularidad mostrando más de su cuerpo. En un encuentro con seguidores, es drogada y llevada a un hotel."
+    }, {
+        id: "H7",
+        titulo: "Juvenile Pornography",
+        poster: "https://iili.io/qA9veNS.png",
+        bg: [
+            "https://iili.io/qA9veNS.png",
+            "https://iili.io/qA9vOR2.png",
+            "https://iili.io/qA9vXKG.png",
+            "https://iili.io/qA9v8x9.png"
+        ],
+        video: [
+            "https://www.burstcloud.co/embed/821b29e25d353dee224273241b84fd908d3871e9e7c4b717c4a5beb508f020c3/ep1.mp4"
+        ],
+        estudio: "Bootleg",
+        fecha: "2020",
+        tags: ["Incesto", "Ninfomana", "Sincensura"],
+        tipo: "sincensura",
+        descripcion: "Mientras el sol se filtra entre los árboles, una hermosa mujer está absorta en su libro. Lee el relato de una chica que se sincera sobre sus deseos, una chica que se retuerce en un placer sin límites."
+    }, {
+        id: "H8",
+        titulo: "Night Tail",
+        poster: "https://iili.io/qIdQMap.png",
+        bg: [
+            "https://iili.io/qIdQMap.png",
+            "https://iili.io/qIdQOns.png",
+            "https://iili.io/qIdQkPf.png",
+            "https://iili.io/qIdQScl.png"
+        ],
+        video: [
+            "https://www.burstcloud.co/embed/acd413209029d267000883873390e52e6b4e12092df5ec5a37a31690501d238e/ep1.mp4",
+            "https://hentaiplayer.com/v/yFPLae8m0N3oPEa/"
+        ],
+        estudio: "Majin Petit",
+        fecha: "2023",
+        tags: ["Eichi", "Monstruos", "Tetonas", "Sincensura"],
+        tipo: "sincensura",
+        descripcion: "Yoru y Asahi dirigen una cafetería durante el día, pero tienen un trabajo secreto como Night Tail para eliminar demonios de los sueños por la noche. Sin embargo, durante sus propios sueños, se involucran en situaciones pervertidas."
+    }, {
+        id: "H9",
+        titulo: "Niku Mesu R30 Nikuyoku ni Ochita Mesu",
+        poster: "https://iili.io/qT3lNwl.jpg",
+        bg: [
+            "https://iili.io/qT3lNwl.jpg",
+            "https://iili.io/qT3lOt2.jpg",
+            "https://iili.io/qT3lknS.jpg",
+            "https://iili.io/qT3liwQ.jpg"
+        ],
+        video: [
+            "https://www.burstcloud.co/embed/90a50d111d2169bfe7eab5bfe8f273604b439c87f09a3e8daa52776f49dadd23/ep1.mp4"
+        ],
+        estudio: "Pink Pineapple",
+        fecha: "2020",
+        tags: ["Bondage", "Milfs", "Orgias", "Sincensura"],
+        tipo: "sincensura",
+        descripcion: "Naoki debe mantenerse virgen hasta los 30 años para heredar el liderazgo familiar, pero las constantes tentaciones a su alrededor ponen a prueba su determinación."
+    }, {
+        id: "H10",
+        titulo: "No Wife No Life",
+        poster: "https://iili.io/qT3iWLQ.png",
+        bg: [
+            "https://iili.io/qT3iWLQ.png",
+            "https://iili.io/qT3ijEB.png",
+            "https://iili.io/qT3iSTJ.png",
+            "https://iili.io/qT3igQR.png"
+        ],
+        video: [
+            "https://www.burstcloud.co/embed/0f17bedacdbc00d22e23aeb423203d76511b90117d723a851ac3a6ce336b3d93/ep1.mp4",
+            "https://www.burstcloud.co/embed/ee0394cad917fe54ff596a7c2f1c182319211567b4a2c48b6b39bf782e6e6a37/ep2.mp4"
+        ],
+        estudio: "Queen Bee",
+        fecha: "2024",
+        tags: ["Bondage", "Milfs", "Orgias", "Sincensura"],
+        tipo: "sincensura",
+        descripcion: "El hentai está ambientado en la Corporación Tateen, donde trabaja el protagonista. Todos los días trabaja como tutor de jovencísimas traini, entre ellas Mitsurugi y Nagihara. Las chicas resultan ser muy capaces y hacen un trabajo excelente."
     },
+
 ];
