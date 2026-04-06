964f3369f319b48ec79485e3a8ec158b44a1d01d
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-04-06 12:13:03 -0500

    Add files via upload

--- hentaibase.js
+++ hentaibase.js
@@ -1,26 +1,6 @@
 const baseDeDatos = [
     { 
         id: "H1", 
-        titulo: "Lover in Law", 
-        poster: "https://iili.io/qn3cHVs.jpg",
-        bg: [
-            "https://iili.io/qn3cHVs.jpg",
-            "https://iili.io/qn3amNt.png",
-            "https://iili.io/qn3cJiG.png",
-            "https://iili.io/qn3cFUl.jpg"
-        ],        
-        video: [
-            "https://www.burstcloud.co/embed/1e51b289259d7a0477853a3b915082e1356f2fab5bac64bee80d2e070d153143/ep1.mp4",
-            "https://www.burstcloud.co/embed/f420104c81a18129c320e2aead139fec63a1968826e5cf74301dfb026bf4bfa1/ep2.mp4"
-        ], 
-        estudio: "Discovery",
-        fecha: "2020", 
-        tags: ["Anal", "Love", "Sincensura"], 
-        tipo: "sincensura", 
-        descripcion: "Tsutomu ayuda a su cuñada Mai con sus deseos..."
-    },
-    { 
-        id: "H2", 
         titulo: "Bakunyuu Oyako", 
         poster: "https://iili.io/qxaeW67.png",
         bg: [
@@ -38,5 +18,85 @@ const baseDeDatos = [
         tags: ["Maid", "Milf", "Sincensura"], 
         tipo: "sincensura", 
         descripcion: "Yusuke se enfrenta a un dilema: su tutoría con la voluptuosa Reina se complica cuando su madre, aún más exuberante, también lo desea. Ambas luchan por ganarse su amor."
-    }
+    },
+	{ 
+        id: "H2", 
+        titulo: "Nightmare x Deathscythe", 
+        poster: "https://iili.io/qI9Oq9S.png", // Imagen para los catálogos
+        bg: [
+            "https://iili.io/qI9Oq9S.png",
+            "https://iili.io/qI9O3F4.png",
+            "https://iili.io/qI9OKS2.png",
+            "https://iili.io/qI9OdPf.png"
+        ],
+        video: [
+            "https://www.burstcloud.co/embed/86f1f74015c506d3026020ba601b605c1fd2dab9fbc9b75d9f2673fa54601f4b/ep1.mp4",
+            "https://www.burstcloud.co/embed/141f8dc8de4ba851b617aed08594b5c53ce728f4338a9bd2fdc73a42356d7ea9/ep2.mp4"
+        ], 
+        estudio: "MediaBank",
+        fecha: "2023", 
+        tags: ["Bondage", "Harem", "Orgias", "Sincensura"], 
+        tipo: "sincensura", 
+        descripcion: "Natsume Rinne, una colegiala que despierta como Shinigami tras vengar a su padre, es capturada por una secta demoníaca. Cuando está a punto de ser sacrificada, las exorcistas Setsuna y Yuuka intentan salvarla, pero son rodeadas."
+    },
+	{ 
+        id: "H3", 
+        titulo: "Seiyoku Tsuyo Tsuyo", 
+        poster: "https://iili.io/qI9yHkN.png", // Imagen para los catálogos
+        bg: [
+            "https://iili.io/qI9yHkN.png",
+            "https://iili.io/qI9y2It.png",
+            "https://iili.io/qI9yFQn.png",
+            "https://iili.io/qI9py2R.png"
+        ],
+        video: [
+            "https://www.burstcloud.co/embed/6be4adad6db4ab45268fe42af5211fd044449d0edcbb7b697f8eddaa9dffb91d/ep1.mp4",
+            "https://www.burstcloud.co/embed/d0860a1e261be788c9a5b007a4928b0da55685edc58e0012f73ac3f691c6f815/ep2.mp4"
+        ], 
+        estudio: "Pink Pineapple",
+        fecha: "2024", 
+        tags: ["Escolares", "Netorare", "Tetonas", "Sincensura"], 
+        tipo: "sincensura", 
+        descripcion: "Nanase, una chica gal insatisfecha con su vida sexual, se burla de Nozaki, su compañero otaku. Planea humillarlo haciéndole mostrar su miembro, pero queda impactada por su tamaño. Su deseo crece y termina usándolo para su propio placer."
+    },
+	{ 
+        id: "H4", 
+        titulo: "Natsu ga Owaru made: Natsu no Owari", 
+        poster: "https://iili.io/qIHu7b1.png", // Imagen vertical para los catálogos
+        bg: [
+            "https://iili.io/qIHu7b1.png",
+            "https://iili.io/qIHu5OP.png",
+            "https://iili.io/qIHuR5B.png",
+            "https://iili.io/qIHuAJV.png"
+        ],
+        video: [
+            "https://www.burstcloud.co/embed/774c0ef44b5268c23022b196e4d4d768e13c47b993dc1c1be5ed1fd9df8e8cf5/ep1.mp4",
+            "https://www.burstcloud.co/embed/40dc0ac0bb8b69bff8f5ff82c2b41aae3ae5a9f8ccfb7e54e11d2feec8d41b9a/ep2.mp4"
+        ], 
+        estudio: "Showten",
+        fecha: "2024", 
+        tags: ["Anal", "Netorare", "Tetonas", "Sincensura"], 
+        tipo: "sincensura", 
+        descripcion: "Es la historia de Yui, una colegiala que acepta el acoso sexual de su maestro pervertido para proteger el sueño de su novio, Kou. Entre la humillación, el sufrimiento y el placer, Yui se convirtió gradualmente en un mero objeto sexual lujurioso."
+    },
+	{ 
+        id: "sefri-no-senpai", 
+        titulo: "SeFri no Senpai", 
+        poster: "https://iili.io/qIHvhpp.jpg", // Imagen vertical para catálogos
+        bg: [
+            "https://iili.io/qIHvhpp.jpg",
+            "https://iili.io/qIHvXvR.jpg",
+            "https://iili.io/qIHvOQt.jpg",
+            "https://iili.io/qIHvkCX.jpg"
+        ],
+        video: [
+            "https://www.burstcloud.co/embed/083a2d152d8fa127205f002ccdb791eb207500a4287cfe147aacbf16396be0c5/ep1.mp4",
+            "https://www.burstcloud.co/embed/0cff9c7f17ba2493a302ea014c2310dbb7dc5071153a07a55459777ea1bc966a/ep2.mp4"
+        ], 
+        estudio: "Pink Pineapple",
+        fecha: "2024", 
+        tags: ["Fantasia", "Oral", "Virgenes", "Sincensura"], 
+        tipo: "sincensura", 
+        descripcion: "Yuuma, un joven universitario, conoce a Nana en su ceremonia de bienvenida. Esa noche, ella aparece en su departamento con la intención de quitarle la virginidad."
+    },
 ];
