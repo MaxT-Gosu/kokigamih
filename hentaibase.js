447e705b83fbe5692c635965bd316dd6e835cee0
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-04-06 10:51:36 -0500

    Add files via upload

---
+++ hentaibase.js
@@ -0,0 +1,17 @@
+const baseDeDatos = [
+    { 
+        id: "lover-in-law", 
+        titulo: "Lover in Law", 
+        poster: "https://iili.io/qn3cHVs.jpg", // Imagen vertical para los catálogos
+        bg: "https://iili.io/qn3amNt.png",      // Única imagen horizontal para el fondo del reproductor
+        video: [
+            "https://www.burstcloud.co/embed/1e1.mp4",
+            "https://www.burstcloud.co/embed/2e2.mp4"
+        ], 
+        estudio: "Discovery",
+        fecha: "2020", 
+        tags: ["Anal", "Love", "Sincensura"], 
+        tipo: "sincensura", 
+        descripcion: "Tsutomu ayuda a su cuñada Mai con sus deseos..."
+    }
+];
