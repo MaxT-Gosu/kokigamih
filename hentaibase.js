8ffedddbee73e65fa92b1a27a51b9ac7c10e9bea
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-04-06 12:06:27 -0500

    Add files via upload

--- hentaibase.js
+++ hentaibase.js
@@ -1,17 +1,42 @@
 const baseDeDatos = [
     { 
-        id: "lover-in-law", 
+        id: "H1", 
         titulo: "Lover in Law", 
-        poster: "https://iili.io/qn3cHVs.jpg", // Imagen vertical para los catálogos
-        bg: "https://iili.io/qn3amNt.png",      // Única imagen horizontal para el fondo del reproductor
+        poster: "https://iili.io/qn3cHVs.jpg",
+        bg: [
+            "https://iili.io/qn3cHVs.jpg",
+            "https://iili.io/qn3amNt.png",
+            "https://iili.io/qn3cJiG.png",
+            "https://iili.io/qn3cFUl.jpg"
+        ],        
         video: [
-            "https://www.burstcloud.co/embed/1e1.mp4",
-            "https://www.burstcloud.co/embed/2e2.mp4"
+            "https://www.burstcloud.co/embed/1e51b289259d7a0477853a3b915082e1356f2fab5bac64bee80d2e070d153143/ep1.mp4",
+            "https://www.burstcloud.co/embed/f420104c81a18129c320e2aead139fec63a1968826e5cf74301dfb026bf4bfa1/ep2.mp4"
         ], 
         estudio: "Discovery",
         fecha: "2020", 
         tags: ["Anal", "Love", "Sincensura"], 
         tipo: "sincensura", 
         descripcion: "Tsutomu ayuda a su cuñada Mai con sus deseos..."
+    },
+    { 
+        id: "H2", 
+        titulo: "Bakunyuu Oyako", 
+        poster: "https://iili.io/qxaeW67.png",
+        bg: [
+            "https://iili.io/qxaeW67.png",
+            "https://iili.io/qxaejae.png",
+            "https://iili.io/qxaevZQ.png",
+            "https://iili.io/qxaeUMB.png"
+        ],
+        video: [
+            "https://www.burstcloud.co/embed/a02abafa62c58e667405ea44b4f81b41055c932a0e2d6c7de236ae3a5a6381d2/ep1.mp4",
+            "https://www.burstcloud.co/embed/91aa09a728bb91e9e7cf63702963042201ea0a8308cd677784b99d18899671a7/ep2.mp4"
+        ], 
+        estudio: "Y.O.U.C.",
+        fecha: "2020", 
+        tags: ["Maid", "Milf", "Sincensura"], 
+        tipo: "sincensura", 
+        descripcion: "Yusuke se enfrenta a un dilema: su tutoría con la voluptuosa Reina se complica cuando su madre, aún más exuberante, también lo desea. Ambas luchan por ganarse su amor."
     }
 ];
