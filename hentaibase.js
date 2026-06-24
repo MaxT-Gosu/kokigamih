6c19d39ac11bb699c9a1d3c7afe57fd401d3a65d
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-06-24 11:20:55 -0500

    Add files via upload

--- hentaibase.js
+++ hentaibase.js
@@ -5719,7 +5719,9 @@ const baseDeDatos = [{
         "https://iili.io/q5H3cAu.png"
     ],
     video: [
-        "https://www.burstcloud.co/embed/f3e34824af620fd7a3cb276499eae0db60d65e199a1030f77a7a2a20e48e5515/ep1.mp4"
+        "https://www.burstcloud.co/embed/52175a6d02178eec2e2e9a737d752c31165ebaa23f704000ef92f91bf9226299/ep1.mp4"
+		"https://www.burstcloud.co/embed/71d9abbad0fbeccc2fcaf3c0dc0f36ce28e9069c7d9755564cf757f98f199e2d/ep2.mp4"
+		
     ],
     estudio: "Magin Label",
     fecha: "2026",
